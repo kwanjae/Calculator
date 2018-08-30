3e5ddda335319554cbff787d16cd708c153eefbf
金寛宰 <KimKwanJae@kinhiroshisai-no-iMac.local>
2018-08-30 20:03:37 +0900

    8/30 commit 1

--- CalculatorApp/ViewController.swift
+++ CalculatorApp/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var formulaLabel: UILabel!
+    @IBOutlet weak var answerLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +21,11 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    @IBAction func inputFormula(_ sender: UIButton) {
+    }
+    @IBAction func clearCalculation(_ sender: UIButton) {
+    }
+    @IBAction func calculateAnswer(_ sender: UIButton) {
+    }
 }
 
