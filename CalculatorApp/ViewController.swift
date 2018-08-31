754d6ce784a77572c7c66f286986e8154c70d21d
竹中寛宰 <hirotada@takenakahiroshisainoMacBook-Pro.local>
2018-08-31 15:00:08 +0900

    8/31 commit 1

--- CalculatorApp/ViewController.swift
+++ CalculatorApp/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Expression
 
 class ViewController: UIViewController {
     @IBOutlet weak var formulaLabel: UILabel!
@@ -34,8 +35,46 @@ class ViewController: UIViewController {
         formulaLabel.text = formulaText + senderedText
     }
     @IBAction func clearCalculation(_ sender: UIButton) {
+        formulaLabel.text = ""
+        answerLabel.text = ""
     }
     @IBAction func calculateAnswer(_ sender: UIButton) {
+        guard let formulaText = formulaLabel.text else {
+            return
+        }
+        let formula: String = formatFormula(formulaText)
+        answerLabel.text = evalFormula(formula)
+    }
+    
+    private func formatFormula(_ formula: String) -> String {
+        let formattedFormula: String = formula.replacingOccurrences(
+            of: "(?<=^|[÷×\\+\\-\\(])([0-9]+)(?=[÷×\\+\\-\\)]|$)",
+            with: "$1.0",
+            options: NSString.CompareOptions.regularExpression,
+            range: nil
+        ).replacingOccurrences(of: "÷", with: "/").replacingOccurrences(of: "×", with: "*")
+        
+        return formattedFormula
+    }
+    
+    private func evalFormula(_ formula: String) -> String {
+        do {
+            let expression = Expression(formula)
+            let answer = try expression.evaluate()
+            return formatAnswer(String(answer))
+        } catch {
+            return "式を正しく入力してください"
+        }
+    }
+    
+    private func formatAnswer(_ answer: String) -> String {
+        let formattedAnswer: String = answer.replacingOccurrences(
+            of: "\\.0$",
+            with: "",
+            options: NSString.CompareOptions.regularExpression,
+            range: nil)
+        
+        return formattedAnswer
     }
 }
 
